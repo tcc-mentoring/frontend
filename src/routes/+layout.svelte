a1ca4a3807b61f03cef7c131ba3eb4118a49dcb6
felipe ricardi <felipericardi5@gmail.com>
2023-02-15 00:41:29 -0300

    chore: removing navbar from entry page and adding to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
+	import { page } from '$app/stores';
     import '@picocss/pico/css/pico.css';
     
 
     import { addMessages, init } from 'svelte-i18n';
+	import Navbar from '../components/navbar/navbar.svelte';
     import pt from '../locale/pt.json';
 
     addMessages("pt", pt);
@@ -13,4 +15,14 @@
     });
 </script>
 
-<slot></slot>
+<style>
+.page-content {
+    padding: 0 20px;
+}
+</style>
+
+<Navbar />
+
+<div class="page-content">
+    <slot></slot>
+</div>
