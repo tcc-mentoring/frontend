d1be4baeb17922314770fe83a4f8955be2a6a200
felipe ricardi <felipericardi5@gmail.com>
2023-02-18 23:45:49 -0300

    feat: adding suppor to fetch current user profile

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,8 +1,25 @@
+import { BACKEND } from "$env/static/private"
 import { redirect } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals, cookies }) => {
     if (!locals.user) {
       throw redirect(302, '/login')
+    }
+    const session = cookies.get('session');
+    const fetchProfile = await fetch(`${BACKEND}profile/user`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${session}`
+        }
+    });
+
+    if (fetchProfile.ok) {
+        const userProfile = await fetchProfile.json();
+
+        return {
+            userProfile
+        }
     }
 }  
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,5 +1,38 @@
 <script lang="ts">
+    import { _ } from 'svelte-i18n'
     import { page } from '$app/stores';
+	import type { PageData } from './$types';
+
+    export let data: PageData;
 </script>
 
+
 <h4>{$page.data.user.firstName}&nbsp;{$page.data.user.lastName}</h4>
+
+<div>
+    <h5>{$_("ocupations")}</h5>
+    <ul>
+        {#if data.userProfile.ocupations.length === 0}
+            <li>{$_("noOcupations")}</li>
+        {/if}
+        {#each data.userProfile.ocupations as ocupation}
+            <li>
+                {ocupation.description}
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<div>
+    <h5>{$_("academyEntries")}</h5>
+    <ul>
+        {#if data.userProfile.academyEntries.length === 0}
+            <li>{$_("noAcademyEntries")}</li>
+        {/if}
+        {#each data.userProfile.academyEntries as academyEntry}
+            <li>
+                {academyEntry.course}
+            </li>
+        {/each}
+    </ul>
+</div>
