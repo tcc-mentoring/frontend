cdd575bc9d6bb7fd4ef22d529942481fc295bf12
felipe ricardi <felipericardi5@gmail.com>
2023-02-20 17:59:43 -0300

    feat: creating component  to handle checkbox input

---
+++ src/components/form/inputCheckbox.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { _ } from 'svelte-i18n'
+
+    export let labelSlug: string;
+    export let name: string;
+    export let checked: boolean;
+
+</script>
+<div class="form-group">
+    <input type="checkbox" {name} bind:checked={checked}/>
+    <label for={name}>{$_(labelSlug)}</label>
+</div>
+
