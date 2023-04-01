6b469cc4c80f426cdbf83737f28f6f9ff965a863
felipe ricardi <felipericardi5@gmail.com>
2023-04-01 20:00:51 -0300

    feat: creating guard to login

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,17 @@
+import { redirect } from '@sveltejs/kit'
 import type { LayoutServerLoad } from './$types'
 
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = async ({ locals, route }) => {
+  if (!locals.user && !loggedOutRoutes.includes(route.id ?? "")) {
+    throw redirect(302, "/login");
+  }
+
   return {
     user: locals.user,
   }
 }
+
+const loggedOutRoutes = [
+  "/login",
+  "/register"
+]
