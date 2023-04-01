53ab7bf442cb063d672742b199221bf79a314ef4
felipe ricardi <felipericardi5@gmail.com>
2023-04-01 19:55:44 -0300

    fix: using new component and updating fields

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { enhance } from "$app/forms";
 	import { _ } from "svelte-i18n";
 	import FormGroup from "../components/form/formGroup.svelte";
+    import MentorCard from "../components/mentor/MentorCard.svelte";
 	import type { MentorProfile } from "../validators/mentorProfileValidator";
 	import type { PageData } from "./$types";
 
@@ -32,47 +33,16 @@
         grid-template-columns: repeat(auto-fill, 350px);
         column-gap: 20px;
     }
-    .specialties {
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-    }
-    .specialties > li {
-        list-style-type: none;
-        background: var(--primary);
-        color: var(--primary-inverse);
-        font-size: 12px;
-        float: left;
-        margin-right: 10px;
-        padding: 5px 10px;
-        border-radius: 3px
-    }
 </style>
 
 <section class="mentor-profiles">
     {#each mentorProfiles as mentorProfile}
-        <article>
-            <header>
-                {mentorProfile.user.email}
-            </header>
-            
-            {$_("mentorProfileSpecialtiesList")}
-            
-            <ul class="specialties">
-                {#each mentorProfile.specialties as specialty}
-                    <li>{specialty}</li>
-                {/each}
-            </ul>
-            
-            <footer>
-                <button class="outline" on:click={() => {openScheduleSessionDialog(mentorProfile)}}>{$_("scheduleSession")}</button>
-            </footer>
-        </article>
+        <MentorCard {mentorProfile} onScheduleClick={openScheduleSessionDialog}/>
     {/each}
 
     <dialog id="scheduleSessionDialog">
         {#if !!mentorProfileToSchedule && !!mentorProfileToSchedule.user}
-            <form action="?/scheduleSession" use:enhance>
+            <form action="?/scheduleSession" method="POST" use:enhance>
                 <article>
                     <header>
                         {$_("scheduleSessionWith", { values: {
@@ -80,13 +50,13 @@
                         } })}
                     </header>
 
-                    <input type="hidden" name="menthor" bind:value={mentorProfileToSchedule.user.email}>
-                    <FormGroup labelSlug="scheduleSession.date" name="date" type="datetime-local"/>
+                    <input type="hidden" name="menthorEmail" bind:value={mentorProfileToSchedule.user.email}>
+                    <FormGroup labelSlug="scheduleSession.date" name="startDatetime" type="datetime-local" extraProperties={{step: (60*30)}}/>
 
                     <footer>
                         <button type="submit">{$_("send")}</button>
 
-                        <button class="outline" on:click={() => {closeScheduleSessionDialog()}}>{$_("close")}</button>
+                        <button class="outline" type="button" on:click={() => {closeScheduleSessionDialog()}}>{$_("close")}</button>
                     </footer>
                 </article>
             </form>
