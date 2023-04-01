f153c65f42457569b9388cfba241b0951f10286f
felipe ricardi <felipericardi5@gmail.com>
2023-04-01 20:00:41 -0300

    feat: fetching mentee sessions to populate calendar

--- src/routes/calendar/+page.server.ts
+++ src/routes/calendar/+page.server.ts
@@ -1,8 +1,18 @@
+import { BACKEND } from "$env/static/private"
 import { redirect } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals, fetch}) => {
     if (!locals.user) {
       throw redirect(302, '/')
     }
+
+    const fetchMenteeSessions = await fetch(`${BACKEND}schedule/mentee-sessions`);
+
+    if (fetchMenteeSessions.ok) {
+      return {
+        menteeSessions: await fetchMenteeSessions.json()
+      }
+    }
+
 }  
