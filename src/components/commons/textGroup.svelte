fbd7bb22e29521eabc215a84c6a54d9b879f1ed9
felipe ricardi <felipericardi5@gmail.com>
2023-04-07 23:30:43 -0300

    feat: creating component to display information

---
+++ src/components/commons/textGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { _ } from "svelte-i18n";
+
+    export let identifier: string;
+    export let labelSlug: string;
+    export let text: string;
+</script>
+
+<div class="text-group">
+    <label for={identifier}>
+        {$_(labelSlug)}
+    </label>
+    <div id={identifier}>
+        {text}
+    </div>
+</div>
