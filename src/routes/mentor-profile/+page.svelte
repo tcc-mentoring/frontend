a85d85dd6807752e22d45c768aade8395d44a715
felipe ricardi <felipericardi5@gmail.com>
2023-04-07 23:32:47 -0300

    feat: wrapping with article

--- src/routes/mentor-profile/+page.svelte
+++ src/routes/mentor-profile/+page.svelte
@@ -2,9 +2,9 @@
     import Tags from "svelte-tags-input";
 	import { enhance } from '$app/forms';
     import { _ } from 'svelte-i18n'
-    import type { ActionData, PageData } from './$types';
+    import type { PageData } from './$types';
 	import TextArea from '../../components/form/textArea.svelte';
-	import { invalidate, invalidateAll } from "$app/navigation";
+	import { invalidate } from "$app/navigation";
 
     export let data: PageData;
     export let specialties: string[] = [];
@@ -20,62 +20,61 @@
     <h2>
         {$_("mentorProfile")}
     </h2>
-
-    {#if data.mentorNotRegistered}
-        <form 
-            action="?/mentorRegister" 
-            method="POST" 
-            use:enhance={() => {
-                return  ({ result, update }) => {
-                    if(result.type === "success") {
-                        invalidate('app:mentor-profile');
-                    } else {
-                        update()
-                    }
-                }
-          }}>
-            <TextArea name="selfDescription" labelSlug="mentorProfileSelfDescription"  error={undefined}/>
-
-            <div class="form-group">
-                <label for="knowledgeArea">{$_('mentorProfileKnowledgeArea')}</label>
-                <select name="knowledgeArea" required>
-                    <option value="BIO">BIO</option>
-                    <option value="ENG">ENG</option>
-                    <option value="SAU">SAU</option>
-                    <option value="AGR">AGR</option>
-                    <option value="LIT">LIT</option>
-                    <option value="SOC">SOC</option>
-                    <option value="HUM">HUM</option>
-                    <option value="EXA">EXA</option>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="specialties">{$_('mentorProfileSpecialties')}</label>
-
-                <Tags 
-                    name="specialties"
-                    bind:tags={specialties}
-                    onlyUnique="true"
-                />
-            </div>
-
-
-            <input type="hidden" bind:value={specialties} name="specialties-value">
-                
-            <button class="submit-profile-information" type="submit">{$_("saveOcupation")}</button>
-
-        </form>
-    {:else}
-        <article>
-            <div>{data.mentorProfile.selfDescription}</div>
-            <div>{data.mentorProfile.knowledgeArea}</div>
-            <ul>
-                {#each data.mentorProfile.specialties as specialty}
-                    <li>{specialty}</li>
-                {/each}
-            </ul>
-        </article>
-    {/if}
     
+    <article>
+        {#if data.mentorNotRegistered}
+            <form 
+                action="?/mentorRegister" 
+                method="POST" 
+                use:enhance={() => {
+                    return  ({ result, update }) => {
+                        if(result.type === "success") {
+                            invalidate('app:mentor-profile');
+                        } else {
+                            update()
+                        }
+                    }
+            }}>
+                <TextArea name="selfDescription" labelSlug="mentorProfileSelfDescription"  error={undefined}/>
+
+                <div class="form-group">
+                    <label for="knowledgeArea">{$_('mentorProfileKnowledgeArea')}</label>
+                    <select name="knowledgeArea" required>
+                        <option value="BIO">BIO</option>
+                        <option value="ENG">ENG</option>
+                        <option value="SAU">SAU</option>
+                        <option value="AGR">AGR</option>
+                        <option value="LIT">LIT</option>
+                        <option value="SOC">SOC</option>
+                        <option value="HUM">HUM</option>
+                        <option value="EXA">EXA</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="specialties">{$_('mentorProfileSpecialties')}</label>
+
+                    <Tags 
+                        name="specialties"
+                        bind:tags={specialties}
+                        onlyUnique="true"
+                    />
+                </div>
+
+
+                <input type="hidden" bind:value={specialties} name="specialties-value">
+                    
+                <button class="submit-profile-information" type="submit">{$_("saveOcupation")}</button>
+
+            </form>
+        {:else}
+                <div>{data.mentorProfile.selfDescription}</div>
+                <div>{data.mentorProfile.knowledgeArea}</div>
+                <ul>
+                    {#each data.mentorProfile.specialties as specialty}
+                        <li>{specialty}</li>
+                    {/each}
+                </ul>
+        {/if}
+    </article>
 </section>
