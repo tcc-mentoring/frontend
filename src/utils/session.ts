dbc29c75d293f0d4290851339dd66f5eb8905515
felipe ricardi <felipericardi5@gmail.com>
2023-04-09 18:47:33 -0300

    feat: updating calendar usage

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,17 +1,13 @@
-export interface SessionDTO { 
-    endDateTime: string; 
-    startDateTime: string; 
-    with: {
-        firstName: string
-    } 
-}
+import type { SessionDTO } from "../validators/sessionValidator";
 
 export function sessionMapToCalendar(session: SessionDTO, role: string, title: string) {
     return {
         end: session.endDateTime, 
         start: session.startDateTime, 
         title: `${title} ${session.with.firstName}`, 
-        backgroundColor: role === "mentor" ? "#5F7470" : "#B8BDB5"
+        backgroundColor: role === "mentor" ? "#5F7470" : "#B8BDB5",
+        id: session.id,
+        extendedProps: session
     }
 }
     
