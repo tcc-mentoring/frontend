835ed8a555a84acc85ffe68d17a0227b2878579d
felipe ricardi <felipericardi5@gmail.com>
2023-05-03 20:06:51 -0300

    ffeat: adding message when not mentor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 
     const SCHEDULE_SESSION_DIALOG_ID = "scheduleSessionDialog";
 
-    let {mentorProfiles} = data;
+    let {mentorProfiles, mentorNotRegistered} = data;
     let mentorProfileToSchedule: MentorProfile = {} as MentorProfile;
 
     let openScheduleSessionDialog = (mentorProfile: MentorProfile) => {
@@ -28,11 +28,13 @@
     }
 </style>
 
+{#if mentorNotRegistered}
+Para se disponibilizar como pessoa mentora, <a href="/mentor-profile">crie seu perfil em "Perfil de Mentor".</a>
+{/if}
 <section class="mentor-profiles">
     {#each mentorProfiles as mentorProfile}
         <MentorCard {mentorProfile} onScheduleClick={openScheduleSessionDialog}/>
     {/each}
-
     <dialog id={SCHEDULE_SESSION_DIALOG_ID}>
         {#if !!mentorProfileToSchedule && !!mentorProfileToSchedule.user}
             <form action="?/scheduleSession" method="POST" use:enhance>
