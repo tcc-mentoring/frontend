5a56beefa0d658c5e752b683b48214a34e2aa502
felipe ricardi <felipericardi5@gmail.com>
2023-05-04 00:09:53 -0300

    feat: creating method to erviewr session

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,4 +1,6 @@
+import type { RequestEvent } from "@sveltejs/kit";
 import type { SessionDTO } from "../validators/sessionValidator";
+import { BACKEND } from "$env/static/private";
 
 export function sessionMapToCalendar(session: SessionDTO, role: string, title: string) {
     return {
@@ -11,3 +13,25 @@ export function sessionMapToCalendar(session: SessionDTO, role: string, title: s
     }
 }
     
+export async function reviewSessionAction({request, fetch}: RequestEvent<Partial<Record<string, string>>, string | null>) {
+    const formData = await request.formData();
+
+    const scheduleId = formData.get("scheduleId");
+    const sessionReviewScore = formData.get("sessionReviewScore");
+    const sessionReviewDetails = formData.get("sessionReviewDetails");
+
+    await fetch(`${BACKEND}schedule/review/${scheduleId}`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        }, 
+        body: JSON.stringify({
+            score: sessionReviewScore,
+            details: sessionReviewDetails
+        })
+    });
+
+    return {
+        success: true
+    }
+}
